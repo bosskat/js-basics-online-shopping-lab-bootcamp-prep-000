c1ff00805490f4651354283efbfec96f9f7ec586
winter mutt <sejongsan@hotmail.com>
2017-12-28 21:19:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ function placeOrder(cardNumber) {
     console.log("We don't have a credit cart on file for you to place your order.");
   }
   else{
-    console.log(`Your total cost is $${total}, which will be charged to the card ${cardNumber}.`);
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
     cart.length = 0;
   }
   return cart;
